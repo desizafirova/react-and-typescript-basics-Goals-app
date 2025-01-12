823b40bd3e64432e93027824967f1daa582ab5ed
DesiZafirova <desszafirowa@gmail.com>
2025-01-12 22:47:44 +0200

    discriminated unions used

--- src/components/InfoBox.tsx
+++ src/components/InfoBox.tsx
@@ -1,22 +1,30 @@
 import { ReactNode } from 'react';
 
-type InfoBoxProps = {
-  mode: 'hint' | 'warning';
+type HintBoxProps = {
+  mode: 'hint';
   children: ReactNode;
 };
 
-export default function InfoBox({ mode, children }: InfoBoxProps) {
-  if (mode === 'hint')
+type WarningBoxProps = {
+  mode: 'warning';
+  severity: 'low' | 'medium' | 'high'; // means that this property is optional
+  children: ReactNode;
+};
+
+type InfoBoxProps = HintBoxProps | WarningBoxProps;
+
+export default function InfoBox(props: InfoBoxProps) {
+  if (props.mode === 'hint')
     return (
-      <aside className="infobox infobox-hint">
-        <p>{children}</p>
+      <aside className={`infobox infobox-hint`}>
+        <p>{props.children}</p>
       </aside>
     );
 
   return (
-    <aside className="infobox infobox-warning warning--medium">
+    <aside className={`infobox infobox-warning warning--${props.severity}`}>
       <h2>Warning</h2>
-      <p>{children}</p>
+      <p>{props.children}</p>
     </aside>
   );
 }
